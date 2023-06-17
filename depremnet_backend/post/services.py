d1d4251108a73c60a7c8be8dd06977a188c5dda2
Orkun Kurul <orkun.kurul@hotmail.com>
2023-06-18 00:29:30 +0300

    update

--- depremnet_backend/post/services.py
+++ depremnet_backend/post/services.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 import jwt
 from rest_framework import exceptions
+import geopy.distance
 
 def parse_jwt_id(token:str) -> int:
     try:
@@ -8,3 +9,14 @@ def parse_jwt_id(token:str) -> int:
         return (payload["id"])
     except Exception:
         raise exceptions.AuthenticalstionFailed("Unauthorized")
+    
+def check_distance(point1, point2, radius):
+    # R-2r < actual distance < R+2r
+    # R: calculated distance between points
+    # r: tolerance radius
+    # res = actual distance
+    # ( r . r ) < R > ( r . r )
+
+    R = geopy.distance.geodesic(point1, point2).m
+    r = 50 # meter 
+    return R-2*r < radius and radius < R+2*r
